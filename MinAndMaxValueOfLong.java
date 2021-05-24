feb1194cee9cf753da98c6bb9f9476d6bb190a03
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-24 14:16:39 +0530

    Add files via upload

---
+++ MinAndMaxValueOfLong.java
@@ -0,0 +1,16 @@
+package Variables;
+
+public class MinAndMaxValueOfLong {
+
+	public static void main(String[] args) {
+
+		long MyLongMinValue = Long.MIN_VALUE;
+		long MyLongMaxValue = Long.MAX_VALUE; 
+		System.out.println("Minimum Integer Value is : "+MyLongMinValue);
+		System.out.println("Maximum Integer Value is : "+MyLongMaxValue);
+		System.out.println("Busted Minimum Integer Value is : "+(MyLongMinValue - 1)); //UnderFlow - If you try to put a value smaller than the minimum value in java , it is called as UnderFlow .
+		System.out.println("Busted Maximum Integer Value is : "+(MyLongMaxValue + 1)); //OverFlow - If you try to to put a value larger than the maximum value in java , it is called as OverFlow .
+		
+	}
+
+}
